d5385ec7fd2784ac711a3b7502f1e0e6aeab121a
Bryan Smith <bryanabsmith@gmail.com>
2018-01-01 10:08:30 -0500

    Added td-idf, descriptives functionality and file output.

--- README.md
+++ README.md
@@ -3,14 +3,18 @@
 This is a simple tool that will analyze a text and produce some insights into the text for qualitative (or quantitative) analysis. This is very much in development so more will come.
 
 ## Features
+* Descriptive analysis
+	* Word count
 * Semantic analysis
+	* Score
+	* Comparative
 
 ## Getting Started
 
 1. Install [Node.js](https://nodejs.org/).
 2. Install all the dependencies by running `npm install` in the directory with the package.json file.
 3. Enjoy.
-4. If you want to try it out, run qualtool against the `test_doc.txt` file provided here.
+	* If you want to try it out, run qualtool against the `test_doc.txt` file provided here.
 
 ## Usage
 The syntax is as follows:
@@ -18,6 +22,25 @@ The syntax is as follows:
 qualtool <name of text file>
 ```
 **_Note: This doesn't work as this hasn't been set up to install as a global module yet. In the meantime, you'll need to run it "locally": `node index.js <name of text file>`._**
+
+## Features
+
+### Descriptives
+* Word count - the number of words in the document.
+* Characters - the number of characters, inclusive of blank space(s).
+* Characters (excl. spaces) - the number of characters, ignoring blank space(s).
+* Average word length - this is the average word length, using the `Characters (excl. spaces)` as the numerator, and `Word count` as the denominator.
+
+### Character counts
+* Counts - the count of each letter in the alphabet in the text, case-insensitive.
+
+### Sentiment
+* Score - the overall score the text's sentiment. Each word (token) is evaluated based on the [AFINN-111](http://www2.imm.dtu.dk/pubdb/views/publication_details.php?id=6010) list and given a score of -5 to 5. Anything lower than 0 is evaluated as a negative word and anything greater than 0 is evaluated as positive (0 is netural). 
+* Comparative - where things "coalesce" on a scale of -5 to 5. Every word has a score ranging from -5 (very negative) to 5 (very positive). This is the "average" score to give you a sense of the strength of the overall positivity or negativity of the text.
+
+### TF-IDF
+* Values - the Term Frequency-Inverse Document Frequency values are listed for each term, and done so in descending order.
+
 ## License
 
 ```
--- index.js
+++ index.js
@@ -1,35 +1,162 @@
+#!/usr/bin/env node
 var fs = require('fs');
 
 var sentiment = require('node-sentiment');
 var ora = require('ora');
 var chalk = require('chalk');
 var table = require('table');
+var wordcount = require('wordcount');
+var natural = require('natural');
 
 var sentScore = 0;
 var aFile = process.argv[2];
+var oFile = process.argv[3];
+
+var naturalTFIDF = natural.TfIdf;
+var tfidf = new naturalTFIDF();
+
+var config = require('./sampleconfig.json'); // Not used yet. Maybe in the future...
+
+var tableDescripData = [
+    ['Property', 'Value']
+];
+
+var tableCharCounts = [
+    ['Letter', 'Count']
+];
+
 var tableSentData = [
     ['Property', 'Value']
 ];
 
-fs.readFile(aFile, 'utf-8', function(err, data) {
+var tfidfData = [
+    ['Term', 'TF-IDF Value']
+];
 
-	var text = data;
+fs.readFile(aFile, 'utf-8', function(err, data) {
+    var writeString = '';
+
 	// https://github.com/sindresorhus/ora/blob/master/readme.md
 	var spinner = ora('Starting analysis of ' + chalk.green(aFile) + '...\n').start();
     setTimeout(function() {
         spinner.color = 'yellow';
         spinner.text = 'Analyzing...';
     }, 1000);
-      
+    
+    writeString += '-- Descriptive Data --\n';
+    var count = wordcount(data);
+    writeString += '\nWord Count: ' + count.toString();
+    var charCountSpaces = data.length;
+    writeString += '\nCharacter Count (with spaces): ' + charCountSpaces.toString();
+    var charCountNoSpaces = data.replace(/ /g, '').length; // https://stackoverflow.com/a/6623263
+    writeString += '\nCharacter Count (excl. spaces): ' + charCountNoSpaces.toString();
+    var avgWordLen = parseFloat(charCountNoSpaces/count).toFixed(2);
+    writeString += '\nAverage word length: ' + avgWordLen.toString();
+
+    tableDescripData.push(['Word Count', count]);
+    tableDescripData.push(['Characters (with spaces)', charCountSpaces]);
+    tableDescripData.push(['Characters (excl. spaces)', charCountNoSpaces]);
+    tableDescripData.push(['Average word length', avgWordLen]);
+
+    var charMap = letterCounts(data);
+    writeString += '\n\n\n-- Character Counts --\n';
+    charMap.forEach(function(val, key, map) {
+        //console.log(key + ' --> ' + val);
+        if (!isNaN(val)) {
+            tableCharCounts.push([key.toString(), val.toString()]);
+            writeString += '\n' + key.toString() + ': ' + val.toString();
+        }
+    });
+    
+
     var sent = sentiment(data);
+    writeString += '\n\n\n-- Sentiment --\n';
+    writeString += '\nScore: ' + sent.score.toFixed(5);
     tableSentData.push(['Score', (sent.score.toFixed(5) > 0) ? chalk.green(sent.score.toFixed(5)) : chalk.red(sent.score.toFixed(5))]);
+    writeString += '\nComparative: ' + sent.comparative.toFixed(5);
     tableSentData.push(['Comparative', (sent.comparative.toFixed(5) > 0) ? chalk.green(sent.comparative.toFixed(5)) : chalk.red(sent.comparative.toFixed(5))]);
+    writeString += '\nVote: ' + sent.vote;
     tableSentData.push(['Vote', sent.vote]);
-	/*console.log('\nScore: ' + sent.score.toFixed(5));
-	console.log('Comparative: ' + sent.comparative.toFixed(5));
-    console.log('Vote: ' + sent.vote);*/
-    var output = table.table(tableSentData);
+
+    // https://github.com/NaturalNode/natural#tf-idf
+    tfidf.addDocument(data);
+    
+    writeString += '\n\n\n-- TD-IDF Values --\n';
+    tfidf.listTerms(0).forEach(function(item) {
+        writeString += '\n' + item.term + ': ' + item.tfidf.toFixed(5);
+        tfidfData.push([item.term, item.tfidf.toFixed(5)]);
+    });
+
+    var outputTdidf = table.table(tfidfData);
+    var outputDesc = table.table(tableDescripData);
+    var outputChar = table.table(tableCharCounts);
+    var outputSent = table.table(tableSentData);
+    
+    console.log('\n' + chalk.bgBlue('Descriptive Analysis\n'));
+    console.log(outputDesc);
+
+    console.log('\n' + chalk.bgBlue('Character Counts\n'));
+    console.log(outputChar);
+
     console.log('\n' + chalk.bgBlue('Sentiment Analysis\n'));
-    console.log(output);
-	spinner.stop();
+    console.log(outputSent);
+
+    console.log('\n' + chalk.bgBlue('TD-IDF Analysis\n'));
+    console.log(outputTdidf);
+
+    
+    
+    console.log(chalk.yellow('Writing results to file...'));
+
+    fs.writeFile(oFile, writeString, function (err) {
+        if (err) throw err;
+        console.log(chalk.green('File written.'));
+        spinner.stop();
+    });
+
 });
+
+function letterCounts(text) {
+    var charMap = new Map([
+        ['a', 0],
+        ['b', 0],
+        ['c', 0],
+        ['d', 0],
+        ['e', 0],
+        ['f', 0],
+        ['g', 0],
+        ['h', 0],
+        ['i', 0],
+        ['j', 0],
+        ['k', 0],
+        ['l', 0],
+        ['m', 0],
+        ['n', 0],
+        ['o', 0],
+        ['p', 0],
+        ['q', 0],
+        ['r', 0],
+        ['s', 0],
+        ['t', 0],
+        ['u', 0],
+        ['v', 0],
+        ['w', 0],
+        ['x', 0],
+        ['y', 0],
+        ['z', 0]
+    ]);
+
+    var splitText = text.split('');
+
+    for (var x = 0; x < splitText.length; x++) {
+        var char = splitText[x];
+        var count = charMap.get(char.toLowerCase());
+        charMap.set(char.toLowerCase(), parseInt(count) + 1);
+        /*if (char !== undefined) {
+            console.log(charMap.get(char.toLowerCase()));
+        }*/
+        //console.log(splitText[x]);
+    }
+    charMap.delete(' '); // Remove the spaces 
+    return charMap;
+}
