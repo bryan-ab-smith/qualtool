fb870b318fec2c9759b044786120e1b15bfb403c
Bryan Smith <bryanabsmith@gmail.com>
2018-01-05 13:20:47 -0500

    Thorough commenting, excel support and parts of speech analysis added.

--- README.md
+++ README.md
@@ -8,6 +8,7 @@ This is a simple tool that will analyze a text and produce some insights into th
 * Semantic analysis
 	* Score
 	* Comparative
+* TF-IDF
 
 ## Getting Started
 
@@ -19,9 +20,10 @@ This is a simple tool that will analyze a text and produce some insights into th
 ## Usage
 The syntax is as follows:
 ```
-qualtool <name of text file>
+qualtool <name of text file> <output file> _<open>_
 ```
-**_Note: This doesn't work as this hasn't been set up to install as a global module yet. In the meantime, you'll need to run it "locally": `node index.js <name of text file>`._**
+_open_ (optional): If you append _open_ to the end of the command, the output file will be opened once the analysis is complete.
+**Note: NPM global installs of qualtool should work...hopefully. Proceed with caution (if concerned, run it locally for now).**
 
 ## Features
 
@@ -43,12 +45,22 @@ qualtool <name of text file>
 
 ## License
 
-```
 Copyright (c) 2017 Bryan Smith
 
-Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the "Software"), to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
+Permission is hereby granted, free of charge, to any person obtaining a copy 
+of this software and associated documentation files (the "Software"), to 
+deal in the Software without restriction, including without limitation the 
+rights to use, copy, modify, merge, publish, distribute, sublicense, and/or 
+sell copies of the Software, and to permit persons to whom the Software is 
+furnished to do so, subject to the following conditions:
 
-The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+The above copyright notice and this permission notice shall be included in 
+all copies or substantial portions of the Software.
 
-THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
-```
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR 
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, 
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE 
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER 
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, 
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE 
+SOFTWARE.
--- index.js
+++ index.js
@@ -1,122 +1,255 @@
 #!/usr/bin/env node
-var fs = require('fs');
 
+// Native module imports.
+var fs = require('fs');
+var cp = require('child_process');
+var os = require('os');
+
+// External modules from NPM.
 var sentiment = require('node-sentiment');
-var ora = require('ora');
 var chalk = require('chalk');
-var table = require('table');
 var wordcount = require('wordcount');
 var natural = require('natural');
+var wpos = require('wordpos');
+var open = require('open');
+var xl = require('excel4node');
 
-var sentScore = 0;
-var aFile = process.argv[2];
-var oFile = process.argv[3];
-
+// Objects for work throughout.
 var naturalTFIDF = natural.TfIdf;
 var tfidf = new naturalTFIDF();
+var wordpos = new wpos();
+var wb = new xl.Workbook();
 
-var config = require('./sampleconfig.json'); // Not used yet. Maybe in the future...
+/*
+    Variables for arguments.
+    - aFile - This is the file that will be analyzed.
+    - oFile - The output file.
+*/
+var aFile = process.argv[2];
+var oFile = process.argv[3];
+var openFile = process.argv[4];
 
-var tableDescripData = [
-    ['Property', 'Value']
-];
+// var config = require('./sampleconfig.json'); // Not used yet. Maybe in the future...
+//var d = new Date();
+/*
+    - This is the counter for how many "functions" there are.
+    - Functions:
+        1. Descriptives - word count, character counts, average word length.
+        2. Character - individual character counts.
+        3. Sentiment - sentiment score, comparative, vote.
+        4. Term Frequency-Inverse Document Frequency - score for each word.
+        5. Parts of Speech - breakdown of nouns, verbs, adjectives, adverbs and the "rest".
+*/
+var catCount = '5';
 
-var tableCharCounts = [
-    ['Letter', 'Count']
-];
+/*
+    - Style for the column headers in the output Excel file.
+    - The colour is a red (with a slight orange twist).
+*/
+var headerStyle = {
+    font: {
+        color: '#BC2100'
+    }
+};
 
-var tableSentData = [
-    ['Property', 'Value']
-];
+// The whole process is wrapped up in a file read callback (for now).
+fs.readFile(aFile, 'utf-8', function (err, data) {
+    /* The following two lines are for a plain text version of output (if the excel output starts to fail in the future and/or I resort back to it).
+        //var dString = d.getFullYear() + '/' + String(parseInt(d.getMonth()) + 1) + '/' + d.getDate() + ', ' + d.getHours() + ':' + d.getMinutes() + ':' + d.getSeconds();
+        //var writeString = '';
+    */
 
-var tfidfData = [
-    ['Term', 'TF-IDF Value']
-];
+    console.log('Starting analysis of ' + chalk.green(aFile) + '...\n');
 
-fs.readFile(aFile, 'utf-8', function(err, data) {
-    var writeString = '';
+    /* 
+        * Descriptive Analysis
+    */
+    process.stdout.write('[1/' + catCount + '] Descriptive analysis...');
+    // Add a wordsheet to our excel file for the descriptive info.
+    var descWS = wb.addWorksheet('Descriptives');
+    // Add the headers.
+    descWS.cell(1, 1).string('Characteristic').style(headerStyle);
+    descWS.cell(1, 2).string('Value').style(headerStyle);
 
-	// https://github.com/sindresorhus/ora/blob/master/readme.md
-	var spinner = ora('Starting analysis of ' + chalk.green(aFile) + '...\n').start();
-    setTimeout(function() {
-        spinner.color = 'yellow';
-        spinner.text = 'Analyzing...';
-    }, 1000);
-    
-    writeString += '-- Descriptive Data --\n';
+    //writeString += 'Qualtool analysis of ' + aFile + '\nDate and Time: ' + dString + '\n\n-- Descriptive Data --\n';
+    // Get the word count for the text.
     var count = wordcount(data);
-    writeString += '\nWord Count: ' + count.toString();
+    //writeString += '\nWord Count: ' + count.toString();
+    // Create the cells for the word count label and the count itself.
+    descWS.cell(2, 1).string('Word Count');
+    descWS.cell(2, 2).number(count);
+
+    // Get the length of the document, inclusive of spaces. 
     var charCountSpaces = data.length;
-    writeString += '\nCharacter Count (with spaces): ' + charCountSpaces.toString();
-    var charCountNoSpaces = data.replace(/ /g, '').length; // https://stackoverflow.com/a/6623263
-    writeString += '\nCharacter Count (excl. spaces): ' + charCountNoSpaces.toString();
-    var avgWordLen = parseFloat(charCountNoSpaces/count).toFixed(2);
-    writeString += '\nAverage word length: ' + avgWordLen.toString();
+    //writeString += '\nCharacter Count (with spaces): ' + charCountSpaces.toString();
+    // Create the cells for the character count.
+    descWS.cell(3, 1).string('Character Count (with spaces)');
+    descWS.cell(3, 2).number(charCountSpaces);
 
-    tableDescripData.push(['Word Count', count]);
-    tableDescripData.push(['Characters (with spaces)', charCountSpaces]);
-    tableDescripData.push(['Characters (excl. spaces)', charCountNoSpaces]);
-    tableDescripData.push(['Average word length', avgWordLen]);
+    // Get the length of the document, without counting whitespace.
+    // Source for regular expression used here: https://stackoverflow.com/a/6623263
+    var charCountNoSpaces = data.replace(/ /g, '').length;
+    //writeString += '\nCharacter Count (excl. spaces): ' + charCountNoSpaces.toString();
+    // Create the cells for the character count, without whitespace.
+    descWS.cell(4, 1).string('Character Count (excl spaces)');
+    descWS.cell(4, 2).number(charCountNoSpaces);
 
+    // Calculate the word length. Here, we divide the no whitespace count by the word count. A rounding choice of two decimal places was chosen here.
+    var avgWordLen = parseFloat(charCountNoSpaces / count).toFixed(2);
+    //writeString += '\nAverage word length: ' + avgWordLen.toString();
+    // Create the cells for the average word length.
+    descWS.cell(5, 1).string('Average word length');
+    descWS.cell(5, 2).number(parseFloat(avgWordLen));
+
+    console.log(chalk.green('done.'));
+
+    /* 
+        * Character Analysis
+    */
+    process.stdout.write('[2/' + catCount + '] Character analysis...');
+
+    // See letterCounts() function below for a description of this. 
     var charMap = letterCounts(data);
-    writeString += '\n\n\n-- Character Counts --\n';
-    charMap.forEach(function(val, key, map) {
-        //console.log(key + ' --> ' + val);
+    //writeString += '\n\n\n-- Character Counts --\n';
+    // Set the variable that serves as the counter for where to insert data into the worksheet. This starts at 2 as row 1 is reserved for the headers.
+    var charCellCount = 2;
+    // Create the worksheet where all the data will go.
+    var charWS = wb.addWorksheet('Characters');
+    // Set up the headers for the worksheet.
+    charWS.cell(1, 1).string('Character').style(headerStyle);
+    charWS.cell(1, 2).string('Count').style(headerStyle);
+
+    // Loop over the map of characters and put them in the worksheet.
+    charMap.forEach(function (val, key, map) {
+        // Here, ignore those characters that have no count whatsoever.
         if (!isNaN(val)) {
-            tableCharCounts.push([key.toString(), val.toString()]);
-            writeString += '\n' + key.toString() + ': ' + val.toString();
+            //writeString += '\n' + key.toString() + ': ' + val.toString();
+            // Insert the letter and its accompanying count into the worksheet.
+            charWS.cell(charCellCount, 1).string(key.toString());
+            charWS.cell(charCellCount, 2).number(parseFloat(val));
+            // Increment the counter.
+            charCellCount++;
         }
     });
-    
+    console.log(chalk.green('done.'));
 
+    /* 
+        * Sentiment Analysis
+    */
+    process.stdout.write('[3/' + catCount + '] Sentiment analysis...');
+    // Get the sentiment of the data through the node-sentiment module.
     var sent = sentiment(data);
-    writeString += '\n\n\n-- Sentiment --\n';
-    writeString += '\nScore: ' + sent.score.toFixed(5);
-    tableSentData.push(['Score', (sent.score.toFixed(5) > 0) ? chalk.green(sent.score.toFixed(5)) : chalk.red(sent.score.toFixed(5))]);
-    writeString += '\nComparative: ' + sent.comparative.toFixed(5);
-    tableSentData.push(['Comparative', (sent.comparative.toFixed(5) > 0) ? chalk.green(sent.comparative.toFixed(5)) : chalk.red(sent.comparative.toFixed(5))]);
-    writeString += '\nVote: ' + sent.vote;
-    tableSentData.push(['Vote', sent.vote]);
+    // Create the worksheet for the sentiment data.
+    var sentWS = wb.addWorksheet('Sentiment');
+    // Insert the headers into the worksheet.
+    sentWS.cell(1, 1).string('Characteristic').style(headerStyle);
+    sentWS.cell(1, 2).string('Value').style(headerStyle);
+    //writeString += '\n\n\n-- Sentiment --\n';
+    //writeString += '\nScore: ' + sent.score.toFixed(5);
+    // Insert the score title and its accompanying value.
+    sentWS.cell(2, 1).string('Score');
+    sentWS.cell(2, 2).number(parseFloat(sent.score.toFixed(5)));
+    //tableSentData.push(['Score', (sent.score.toFixed(5) > 0) ? chalk.green(sent.score.toFixed(5)) : chalk.red(sent.score.toFixed(5))]);
+    //writeString += '\nComparative: ' + sent.comparative.toFixed(5);
+    // Insert the comparative title and its accompanying value.
+    sentWS.cell(3, 1).string('Comparative');
+    sentWS.cell(3, 2).number(parseFloat(sent.comparative.toFixed(5)));
+    //tableSentData.push(['Comparative', (sent.comparative.toFixed(5) > 0) ? chalk.green(sent.comparative.toFixed(5)) : chalk.red(sent.comparative.toFixed(5))]);
+    //writeString += '\nVote: ' + sent.vote;
+    // Insert the vote title and its accompanying value.
+    sentWS.cell(4, 1).string('Vote');
+    sentWS.cell(4, 2).string(sent.vote);
+    //console.log('[3/' + catCount + '] Sentiment analysis ' + chalk.green('done') + '.');
+    console.log(chalk.green('done.'));
+    //tableSentData.push(['Vote', sent.vote]);
 
-    // https://github.com/NaturalNode/natural#tf-idf
+    /* 
+        * TF-IDF Analysis
+    */
+    process.stdout.write('[4/' + catCount + '] Term frequency-inverse document frequency analysis...');
+    //writeString += '\n\n\n-- TD-IDF Values --\n';
+    // Add a worksheet for tf-idf analysis.
+    var tfidfWS = wb.addWorksheet('TF-IDF');
+    // Add headers to the worksheet.
+    tfidfWS.cell(1, 1).string('Word').style(headerStyle);
+    tfidfWS.cell(1, 2).string('Value').style(headerStyle);
+
+    // Set the counter that determines where rows are inserted into the spreadsheet. This starts at 2 as row 1 is reserved for the headers.
+    var tfidfCellCount = 2;
+    // Add the text (aFile) as a document for the natural node module.
+    // Source: https://github.com/NaturalNode/natural#tf-idf
     tfidf.addDocument(data);
-    
-    writeString += '\n\n\n-- TD-IDF Values --\n';
-    tfidf.listTerms(0).forEach(function(item) {
-        writeString += '\n' + item.term + ': ' + item.tfidf.toFixed(5);
-        tfidfData.push([item.term, item.tfidf.toFixed(5)]);
+    // Loop through the terms in the list for document 0 (you can add multiple documents but since we only have one, we only need to loop through document 0).
+    tfidf.listTerms(0).forEach(function (item) {
+        //writeString += '\n' + item.term + ': ' + item.tfidf.toFixed(5);
+        // Add cells for the term and the tf-idf score, rounded to 5 decimal places.
+        tfidfWS.cell(tfidfCellCount, 1).string(item.term);
+        tfidfWS.cell(tfidfCellCount, 2).number(parseFloat(item.tfidf.toFixed(5)));
+        // Increment the row counter.
+        tfidfCellCount++;
+        //tfidfData.push([item.term, item.tfidf.toFixed(5)]);
     });
+    console.log(chalk.green('done.'));
 
-    var outputTdidf = table.table(tfidfData);
-    var outputDesc = table.table(tableDescripData);
-    var outputChar = table.table(tableCharCounts);
-    var outputSent = table.table(tableSentData);
-    
-    console.log('\n' + chalk.bgBlue('Descriptive Analysis\n'));
-    console.log(outputDesc);
+    /* 
+        * Parts of Speech Analysis
+    */
+    process.stdout.write('[5/' + catCount + '] Parts of speech analysis...');
+    //writeString += '\n\n\n-- Parts of Speech Values --\n';
+    // Add a worksheet for the parts of speech analysis.
+    var posWS = wb.addWorksheet('Parts of Speech');
+    // Get the parts of speech (POS) - nouns, verbs, adjectives, adverbs and "the rest."
+    wordpos.getPOS(data, function (result) {
+        // These variables serves as counts for each POS. 
+        // All qualtool does now is provide counts. There is certainly room, in the future, of doing things with the words themselves.
+        var nouns = result.nouns.length;
+        var verbs = result.verbs.length;
+        var adj = result.adjectives.length;
+        var adv = result.adverbs.length;
+        var rest = result.rest.length;
 
-    console.log('\n' + chalk.bgBlue('Character Counts\n'));
-    console.log(outputChar);
+        // Get the total.
+        var total = nouns + verbs + adj + adv + rest;
+        // Add a header row.
+        posWS.cell(1, 1).string('Part of Speech').style(headerStyle);
+        posWS.cell(1, 2).string('Percentage').style(headerStyle);
+        // Insert rows for each POS and its respective percentage (of the total).
+        posWS.cell(2, 1).string('Nouns');
+        posWS.cell(2, 2).number((parseFloat(nouns) / parseFloat(total)).toFixed(4) * 100);
+        posWS.cell(3, 1).string('Verbs');
+        posWS.cell(3, 2).number((parseFloat(verbs) / parseFloat(total)).toFixed(4) * 100);
+        posWS.cell(4, 1).string('Adjectives');
+        posWS.cell(4, 2).number((parseFloat(adj) / parseFloat(total)).toFixed(4) * 100);
+        posWS.cell(5, 1).string('Adverbs');
+        posWS.cell(5, 2).number((parseFloat(adv) / parseFloat(total)).toFixed(4) * 100);
+        posWS.cell(6, 1).string('Rest');
+        posWS.cell(6, 2).number((parseFloat(rest) / parseFloat(total)).toFixed(4) * 100);
 
-    console.log('\n' + chalk.bgBlue('Sentiment Analysis\n'));
-    console.log(outputSent);
+        /*writeString += '\nNouns: ' + String(nouns) + ' (' + String((parseFloat(nouns)/parseFloat(total)).toFixed(4)*100) + '%)';
+        writeString += '\nVerbs: ' + String(verbs) + ' (' + String((parseFloat(verbs)/parseFloat(total)).toFixed(4)*100) + '%)';
+        writeString += '\nAdjectives: ' + String(adj) + ' (' + String((parseFloat(adj)/parseFloat(total)).toFixed(4)*100) + '%)';
+        writeString += '\nAdverbs: ' + String(adv) + ' (' + String((parseFloat(adv)/parseFloat(total)).toFixed(4)*100) + '%)';
+        writeString += '\nRest: ' + String(rest) + ' (' + String((parseFloat(rest)/parseFloat(total)).toFixed(4)*100) + '%)';*/
+        console.log(chalk.green('done.'));
 
-    console.log('\n' + chalk.bgBlue('TD-IDF Analysis\n'));
-    console.log(outputTdidf);
 
-    
-    
-    console.log(chalk.yellow('Writing results to file...'));
-
-    fs.writeFile(oFile, writeString, function (err) {
-        if (err) throw err;
-        console.log(chalk.green('File written.'));
-        spinner.stop();
+        console.log(chalk.yellow('\nFinishing...'));
+        // Write the file.
+        wb.write(oFile);
+        // If the user appends 'open' to the end of the qualtool command, open the file.
+        if (openFile == 'open') {
+            open(oFile);
+        }
     });
-
 });
 
+/*
+    This function serves to create a JS map that contains counts for how often the character appears.
+        - NOTE: Only letters of the alphabet are included and the count is case-insensitive (for now).
+        - Arguments: the text that serves as the source of the analysis. 
+*/
 function letterCounts(text) {
+    // Set up the map that will house the counts.
     var charMap = new Map([
         ['a', 0],
         ['b', 0],
@@ -146,17 +279,20 @@ function letterCounts(text) {
         ['z', 0]
     ]);
 
+    // Split the text into its constituent parts.
     var splitText = text.split('');
 
+    // Loop over each character in the text.
     for (var x = 0; x < splitText.length; x++) {
+        // Get a character.
         var char = splitText[x];
+        // Get the current count for the character that we're looking at.
         var count = charMap.get(char.toLowerCase());
+        // Increment the count.
         charMap.set(char.toLowerCase(), parseInt(count) + 1);
-        /*if (char !== undefined) {
-            console.log(charMap.get(char.toLowerCase()));
-        }*/
-        //console.log(splitText[x]);
     }
-    charMap.delete(' '); // Remove the spaces 
+    // Remove the spaces 
+    charMap.delete(' ');
+    // Return the character map.
     return charMap;
 }
